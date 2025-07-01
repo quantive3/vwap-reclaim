172a9d6303a5e78bcba5d5d2805271203837a44f
shawnjoshi <shawn@quantive.co>
2025-07-01 16:24:23 -0600

    Switch from SQLite to PostgreSQL

--- check_db.py
+++ check_db.py
@@ -2,11 +2,14 @@ import optuna
 import matplotlib.pyplot as plt
 from optuna.trial import TrialState
 from smart import ValidCountTPESampler
+from optuna.storages import RDBStorage
+from smart import POSTGRES_URL
 
 # Load the study from SQLite database
+storage = RDBStorage(url=POSTGRES_URL)
 study = optuna.load_study(
-    study_name="vwap_bounce_optimization", 
-    storage="sqlite:///vwap.db"
+    study_name="vwap_bounce_optimization",
+    storage=storage,
 )
 
 from collections import defaultdict
